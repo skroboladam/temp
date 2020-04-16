06cd8aa4ccd70440562779732553f14a35eb4d47
skroboladam <skroboladam@gmail.com>
2020-04-16 14:37:28 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -4,8 +4,8 @@ $('.lp-box-oferta .arrow').click(function(e){
 
 e.preventDefault();
 var onklik = $(this).attr("onclick").match(/(\d)/g).toString(); //zapisz numer docelowego elementu do $onklik
-$('.lp-box-oferta .arrow').removeClass('arrow-right') //usuń nową strzałkę z pozostałych
-$(this).addClass('arrow-right') //nadaj klikowi klasę z nową strzałką
+$('.lp-box-oferta .arrow').removeClass('arrow-right'); //usuń nową strzałkę z pozostałych
+$(this).addClass('arrow-right'); //nadaj klikowi klasę z nową strzałką
 
 
 $('[id^="divContent-"]').not("#divContent-"+onklik).each(function(){ //ukryj wszystkie gdzie id zaczyna się od divContent- oprócz klikanego
